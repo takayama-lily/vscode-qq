e0da48cbe8d98aa7d0a7173c833f7c266247ab42
takayama <takayama@foxmail.com>
2021-05-18 02:57:08 +0900

    add no referrer

--- src/chat.ts
+++ src/chat.ts
@@ -50,6 +50,7 @@ function getHtml(id: string, webview: vscode.Webview) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="referrer" content="never">
     <link rel="stylesheet" type="text/css" href="${css}" />
 </head>
 <body>
