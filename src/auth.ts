a80e3064cdd0c8765878b1733ee1128ee454c411
SteveXMH <stevexmh@qq.com>
2021-05-19 06:43:16 +0800

    added more code

--- src/auth.ts
+++ src/auth.ts
@@ -7,20 +7,33 @@ import * as vscode from 'vscode';
 import { AuthenticationProvider, AuthenticationProviderAuthenticationSessionsChangeEvent, AuthenticationSession, EventEmitter } from 'vscode';
 
 class AccountManager implements AuthenticationProvider {
+
+    private static readonly session: AuthenticationSession = {
+        id: '',
+        accessToken: '',
+        scopes: [],
+        // 这个字段会被覆盖后提供给其他接口，应该可以实现多账户
+        account: {
+            id: '',
+            label: ''
+        }
+    };
+
     _onDidChangeSessions =
         new EventEmitter<AuthenticationProviderAuthenticationSessionsChangeEvent>();
     readonly onDidChangeSessions = this._onDidChangeSessions.event;
 
-    getSessions(): Promise<ReadonlyArray<AuthenticationSession>> {
-        return Promise.resolve([]);
-    }
-
-    async removeSession(): Promise<void> {
+    async removeSession(_sessionId: string): Promise<void> {
 
     }
 
-    async createSession(): Promise<AuthenticationSession> {
+    // VSCode 的验证可以指定一个授权范围，不过这里只是 QQ 的账户所以应该不用特别限制
+    async createSession(_scopes: string[]): Promise<AuthenticationSession> {
         throw new Error();
+    }
+
+    getSessions(_scopes?: string[]): Promise<ReadonlyArray<AuthenticationSession>> {
+        return Promise.resolve([]);
     }
 }
 
